f5229dfa348d2fa5c9110b7991310e5c5f861422
Kris Kwiatkowski <contact@amongbytes.com>
2026-02-04 19:42:13 +0000

    Fixes ARMCC build

--- cmake/postbuild.cmake
+++ cmake/postbuild.cmake
@@ -1,15 +1,17 @@
 function(target_postbuild_executable tgt)
-    set(ELF_FILE ${CMAKE_CURRENT_BINARY_DIR}/$<TARGET_FILE_BASE_NAME:${tgt}>)
-    set(HEX_FILE ${ELF_FILE}.hex)
-    set(SECTIONS_FILE ${ELF_FILE}.sections)
-    set(LST_FILE ${ELF_FILE}.lst)
-    set(BIN_FILE ${ELF_FILE}.bin)
+    set(ELF_FILE $<TARGET_FILE:${tgt}>)
+    set(ELF_BASE $<TARGET_FILE_DIR:${tgt}>/$<TARGET_FILE_BASE_NAME:${tgt}>)
+
+    set(HEX_FILE ${ELF_BASE}.hex)
+    set(SECTIONS_FILE ${ELF_BASE}.sections)
+    set(LST_FILE ${ELF_BASE}.lst)
+    set(BIN_FILE ${ELF_BASE}.bin)
 
     if(CMAKE_C_COMPILER_ID STREQUAL "GNU")
         add_custom_command(
             TARGET ${tgt} POST_BUILD
             COMMAND ${OBJCOPY} -Obinary ${ELF_FILE} ${BIN_FILE}
-            COMMAND ${OBJCOPY} -Oihex ${ELF_FILE} ${HEX_FILE}
+            COMMAND ${OBJCOPY} -Oihex   ${ELF_FILE} ${HEX_FILE}
             COMMENT "Invoking: objcopy")
 
         add_custom_command(
@@ -17,12 +19,13 @@ function(target_postbuild_executable tgt)
             COMMAND ${OBJDUMP} -h -D ${ELF_FILE} > ${SECTIONS_FILE}
             COMMAND ${OBJDUMP} -S --disassemble ${ELF_FILE} > ${LST_FILE}
             COMMENT "Invoking: objdump")
+
     elseif(CMAKE_C_COMPILER_ID STREQUAL "ARMCC")
         add_custom_command(
             TARGET ${tgt} POST_BUILD
             COMMENT "Invoking: arm-fromelf"
-            COMMAND ${ARM_FROMELF} --bin --output=${BIN_FILE} $<TARGET_FILE:${tgt}>
-            COMMAND ${ARM_FROMELF} --i32 --output=${HEX_FILE} $<TARGET_FILE:${tgt}>)
+            COMMAND ${ARM_FROMELF} --bin --output=${BIN_FILE} ${ELF_FILE}
+            COMMAND ${ARM_FROMELF} --i32 --output=${HEX_FILE} ${ELF_FILE})
     endif()
 
     set_property(
